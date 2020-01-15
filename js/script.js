cac10f3996c90b48f1966fb7b086bdcc871c5033
Marta Blaszczak <marta.blaszczak1@gmail.com>
2020-01-15 20:56:03 +0000

    add progress bar circle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
+  let numberOne = 21;
+  let numberTwo = 28;
+document.getElementById("allCalls").innerHTML = "/ " + numberTwo ;
+
+$('.counter-inside').each(function() {
+  let $this = $(this),
+      countTo = numberOne;
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 2000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+    }
+
+  }); 
+
+});
+
+
+
 window.onload = function onLoad() {
-  var progressBar = 
-    new ProgressBar.Circle('#progress', {
-      color: 'red',
-      strokeWidth: 10,
-      duration: 2000, // milliseconds
+  let progressBar = 
+    new ProgressBar.Circle('#progress-circle', {
+      color: 'orange',
+      strokeWidth: 6,
+      trailColor: "grey",
+      trailWidth: 6,
+      duration: 2000,
       easing: 'easeInOut'
     });
-
-  progressBar.animate(0.63); // percent
+  
+  let percent = numberOne/numberTwo;
+  progressBar.animate(percent);
 };
+
